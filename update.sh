68308aed81b5352ea28f960bfde5207644e24bd9
lanyue2024 <144880561+lanyue2024@users.noreply.github.com>
2024-09-20 10:07:56 +0800

    add update script

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+file="$1"
+tmpfile=$(mktemp)
+dnsmasq="dnsmasq.conf"
+adguardhome="adguardhome.txt"
+t=$(date)
+
+if [ -f "$file" ]; then
+    echo "# $t" >$dnsmasq
+    echo "" >>$dnsmasq
+    echo "address=/sniproxy.local/IP" >>$dnsmasq
+    echo "" >>$dnsmasq
+
+    echo "# $t" >$adguardhome
+    echo "" >>$adguardhome
+
+    cat $file |tr -d '[:blank:]'|sort -u |egrep -v '^#|^$' >$tmpfile
+    while IFS='' read -r line; do
+        echo "cname=${line},sniproxy.local,10" >>$dnsmasq
+        echo "||${line}^\$dnsrewrite=sniproxy.local" >>$adguardhome
+    done < "$tmpfile"
+fi
+
+rm $tmpfile
+
