a90797eff127da46a46ad3679399c91fbd982e2f
lanyue2024 <144880561+lanyue2024@users.noreply.github.com>
2025-02-17 10:33:39 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,6 +4,7 @@ file="hosts.txt"
 tmpfile=$(mktemp)
 dnsmasq="dnsmasq.conf"
 adguardhome="adguardhome.txt"
+adguardhome100="adguardhome100.txt"
 t=$(date)
 
 if [ -f "$file" ]; then
@@ -15,10 +16,14 @@ if [ -f "$file" ]; then
     echo "# $t" >$adguardhome
     echo "" >>$adguardhome
 
+    echo "# $t" >$adguardhome100
+    echo "" >>$adguardhome100
+
     cat $file |tr -d '[:blank:]'|sort -u |egrep -v '^#|^$' >$tmpfile
     while IFS='' read -r line; do
         echo "cname=${line},sniproxy.local,10" >>$dnsmasq
         echo "||${line}^\$dnsrewrite=sniproxy.local" >>$adguardhome
+        echo "||${line}^\$client=100.0.0.0/8,dnsrewrite=sniproxy100.local" >>$adguardhome100
     done < "$tmpfile"
 fi
 
